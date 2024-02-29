dfa4303e2cb177823460e46b1c20cff609eaf4e3
Bull <dbtjrdla2056@gmail.com>
2024-02-29 22:59:27 +0900

    list page - modify delete widget

--- lib/app/screens/main_page.dart
+++ lib/app/screens/main_page.dart
@@ -6,7 +6,9 @@ import '../widgets/navigation/main_bar_drawer.dart';
 import '../widgets/textfield/search_textfield.dart';
 import '../widgets/appbar/main_appbar.dart';
 import '../widgets/bottombar/list_tap_bar.dart';
+import '../widgets/bottombar/delete_bar.dart';
 import '../../providers/nav_tap_provider.dart';
+import '../../providers/theme_provider.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -19,15 +21,16 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final navTapState = Provider.of<NavTapState>(context);
+    final isSelectionMode = Provider.of<UIState>(context).isSelectionMode;
     return Scaffold(
-            //AppBar
+      //AppBar
       appBar: MainAppbar(
         bottom: navTapState.selectedIndex == 0 ? SearchTextfield() : null,
       ),
       drawer: MainAppDrawer(),
-      backgroundColor: Colors.white,      
+      backgroundColor: Colors.white,
       body: navTapState.navIndex.elementAt(navTapState.selectedIndex),
-      bottomNavigationBar: ListTapBar(),
+      bottomNavigationBar: isSelectionMode ? DeleteBar() : ListTapBar(),
     );
   }
 }
